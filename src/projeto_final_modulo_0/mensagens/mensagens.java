c14d6947c9b8834d4185bc724132915bd1be1d95
brucmarqs <brucmarqs@gmail.com>
2024-07-11 18:49:09 -0300

    Branch Menu

---
+++ src/projeto_final_modulo_0/mensagens/mensagens.java
@@ -0,0 +1,32 @@
+package projeto_final_modulo_0.mensagens;
+
+public class mensagens {
+    public static void sobre () {
+        System.out.println("********************************");
+        System.out.println("Projeto Desenvolvido por: ");
+        System.out.println("Bruna Marques - brucmarqs@gmail.com");
+        System.out.println("********************************");
+
+    }
+    public static void entradaMenu () {
+        System.out.println("********************************");
+        System.out.println("                                ");
+        System.out.println("          PARFUM STORE          ");
+        System.out.println("                                ");
+        System.out.println("********************************");
+        System.out.println("                                ");
+        System.out.println(" 1- Criar Login                 ");
+        System.out.println(" 2- Listar Perfumes             ");
+        System.out.println(" 3- Buscar Perfume por Nome     ");
+        System.out.println(" 4- Sacola de Compras           ");
+        System.out.println(" 5- Pedidos                     ");
+        System.out.println(" 6- Lista de Desejos            ");
+        System.out.println(" 7- Suporte                     ");
+        System.out.println(" 8- Sair                        ");
+        System.out.println("                                ");
+        System.out.println("********************************");
+        System.out.println("Entre com a opção desejada:     ");
+        System.out.println("                                ");
+
+    }
+}
